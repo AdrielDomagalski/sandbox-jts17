40d1932d43f79454f99fa28eff38ad514b1bf0d0
Adriel Domagalski <adriel.soares3@gmail.com>
2017-06-14 15:54:39 -0300

    apptwitter

--- AppTwitter/src/main/java/com/github/adrieldomagalski/java/AppTwitter/AppTwitterApplication.java
+++ AppTwitter/src/main/java/com/github/adrieldomagalski/java/AppTwitter/AppTwitterApplication.java
@@ -3,10 +3,10 @@ package com.github.adrieldomagalski.java.AppTwitter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class AppTwitterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppTwitterApplication.class, args);
+		SpringApplication.run(Twittes.class, args);
 	}
 }
